991cb37f1191549ea1ee457075db5570a15d2e8b
Mackeloss <Mackeloss@gmail.com>
2017-03-15 16:35:40 +0100

    Game Menu

---
+++ app/src/main/java/network/iut/org/flappydragon/game/menu/GameMenu.java
@@ -0,0 +1,16 @@
+package network.iut.org.flappydragon.game.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import network.iut.org.flappydragon.R;
+
+public class GameMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_menu);
+    }
+
+}
